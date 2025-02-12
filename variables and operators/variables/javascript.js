cade404ac20de2b2bc34e3819dcba7ca7c252d58
Carlos <carlosmrd2001@gmail.com>
2025-02-12 16:24:06 -0300

    Update Variaveis

---
+++ variables and operators/variables/javascript.js
@@ -0,0 +1,44 @@
+let name = "Evelyn"
+let surname = "Chevalier"
+
+console.log(name);
+console.log(surname);
+//Variáves designadas por let podem ser re-designadas
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+//Variáves designadas por let podem ser re-designadas. A linha 13 retorna o valor 54.
+
+//const pi = 3.14;
+//pi = 10;
+
+//console.log(pi);
+//Variáveis designadas por const não podem ser re-designadas. O código acima resulta em erro.
+
+const number = (3 + 2) - 76 * (1 + 1);
+console.log(number);
+
+console.log(23 + 97);
+console.log(23+ 24 + 25 + 26 + 27 + 28);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max;
+console.log(percentage);
+
+console.log(100 / "Apple");
+console.log(100 / "10");
+let stringNumber = "10";
+console.log(100 + Number(stringNumber));
