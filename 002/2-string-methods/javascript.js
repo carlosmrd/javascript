48ea13afbac1eeda3a198d77d776c499d4125881
Carlos <carlosmrd2001@gmail.com>
2025-02-14 17:01:42 -0300

    Update Strings e Conditionals

--- 002/2-string-methods/javascript.js
+++ 002/2-string-methods/javascript.js
@@ -52,3 +52,80 @@ console.log(part)
 part = text.substr(-4);
 console.log(part)
 //Se o parâmetro for negativo, a contagem começa no final da string.
+
+let text1 = "Hello, World!";
+let text2 = text1.toUpperCase();
+console.log(text2);
+//toUpperCase() converte uma string para letras maíusculas.
+
+text2 = text1.toLowerCase();
+console.log(text2);
+//toLowerCase() converte uma string para letras minúsculas.
+
+text1 = "Hello,";
+text2 = "World!";
+let text3 = text1.concat(" ", text2);
+console.log(text3);
+//O método concat() junta duas ou mais strings.
+
+text = text1 + " " + text2;
+console.log(text);
+text = text1.concat(" ", text2);
+console.log(text);
+//Pode ser o usado o concat() ou o operador +. Ambas as linhas acima produzem o mesmo resultado.
+
+text1 = "   Hello, World!   ";
+text2 = text1.trim();
+console.log(text2);
+//O método trim() remove espaços em branco no início e no final da string.
+
+text2 = text1.trimStart();
+console.log(text2);
+//O método trimStart() remove espaços em branco no início da string.
+
+text2 = text1.trimEnd();
+console.log(text2);
+//O método trimEnd() remove espaços em branco no final da string.
+
+text = "5"
+let padded = text.padStart(4,"0");
+console.log(padded);
+//O método padStart() completa uma string com um carctere especificado no início até que ela atinga um certo tamanho. Nesse exemplo, ela completa a string com o caractere 0 até que ela tenha 4 caracteres.
+
+text = "5"
+padded = text.padEnd(4,"x");
+console.log(padded);
+//O método padEnd() completa uma string com um carctere especificado no final até que ela atinga um certo tamanho. Nesse exemplo, ela completa a string com o caractere x até que ela tenha 4 caracteres.
+
+let numb= 5;
+text = numb.toString();
+padded = text.padStart(4,"x");
+console.log(padded);
+//Os métodos padStart() e padEnd() só funcionam com strings. Para uso de números, é necessário convertê-los para string primeiro.
+
+text = "Hello world!";
+let result = text.repeat(3);
+console.log(result);
+//A string repeat() retora uma nova string com cópias da string original. O parâmetro count dentro da sintaxe string.repeat(count) define o número de cópias.
+
+text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+console.log(newText)
+//O método replace() retorna uma string com parte da string original substituida. Por padrão ele substitui somente o primeiro resultado encontrado e é sensível a caracteres maiúsculos ou minúsculos.
+
+text = "Please visit Microsoft, and also Microsoft!";
+newText = text.replace(/Microsoft/g, "W3Schools");
+console.log(newText)
+//A flag /g faz com que todos os resultados sejam substituídos.
+
+text = "Please visit Microsoft!";
+newText = text.replace(/MICROSOFT/i, "W3Schools");
+console.log(newText)
+//A flag /i faz com que o método não seja sensível a caracteres maiúsculos ou minúsculos.
+
+text = "I love cats. Cats are very easy to love. Cats are very popular."
+text = text.replaceAll("Cats","Dogs");
+text = text.replaceAll("cats","dogs");
+console.log(text);
+//O método replaceAll() substitui expressões regulares, ao invés de substituir a string. Se o parâmetro for uma expressão regular, a flag /g deve ser utilizada.
+
---
+++ 002/3-conditionals/javascript.js
@@ -0,0 +1,13 @@
+console.log(2 > 1); // true
+console.log(2 == 1); // false
+console.log(2 != 1); // true
+//Em JavaScript comparações matemáticas são escritas da seguinte forma:
+//Maior/Menor que: a > b, a < b.
+//Maior/Menor que ou igual: a >= b, a <= b.
+//Igual: a == b. Importante o uso de dois sinais de igual em ==, que faz referência a teste de equalidade. O uso de um = faz referência a uma atribuição.
+//Não igual: a != b.
+//O resultado das comparações é sempre um valor booleano, true ou false.
+
+let result = 5 > 4;
+console.log(result); // true
+//A comparação pode ser atribuída a uma variável.
